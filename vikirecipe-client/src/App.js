fa4a9ddaaa32b34b00ca790eecf54ec22568de68
Svirin Victoria <svirin.victoria@gmail.com>
2023-10-23 13:13:28 +0200

    connection server- client is established

--- vikirecipe-client/src/App.js
+++ vikirecipe-client/src/App.js
@@ -1,37 +1,38 @@
-import { useProxy } from './hooks/useProxy';
-import './App.css';
-import { useEffect, useState } from 'react';
+import { useProxy } from "./hooks/useProxy";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const proxy = useProxy();
+  const [collectionJsonObject, setCollectionJsonObject] = useState([]);
 
-  const proxy= useProxy();
-  const [collectionJsonObject, setCollectionJsonObject] = useState("Ingredients collection"); 
-
-  useEffect(()=>{
-    async function fetchData(){
-      const ingredCollection = await proxy.bringIngredPr();//bringing ingredients from the server
-      setCollectionJsonObject(ingredCollection)
+  useEffect(() => {
+    async function fetchData() {
+      const ingredCollection = await proxy.bringIngredPr(); //bringing ingredients from the server
+      setCollectionJsonObject(ingredCollection);
+      // console.log( ingredCollection);
     }
     fetchData();
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
-  // function buttonClickHandler(event){
-  //   console.log("button was clicked");
+  function buttonClickHandler(event) {
+    console.log("button was clicked");
+  }
 
-  // }
+  const collectionReactElements = collectionJsonObject.map((item, index) => {
+    return <div key={index} className="collection-style">{item.name}</div>;
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-          Viki recipe - My recipe's book
-        </p>
-   
+        <p>Viki recipe - My recipe's book</p>
       </header>
-      <div className='gray'>
-        {collectionJsonObject}
-        {/* <button onClick={buttonClickHandler}>Click</button> */}
+      <div className="gray">
+        {collectionReactElements}
+        <button onClick={buttonClickHandler}>Click</button>
       </div>
     </div>
   );
--- vikirecipe-client/src/hooks/useProxy.jsx
+++ vikirecipe-client/src/hooks/useProxy.jsx
@@ -1,18 +1,19 @@
-export const useProxy = (props) =>{
-
-    //bringing ingredients from database
-    async function bringIngredPr(){
-        try{
-            const fillIngreds = await fetch("http://localhost:3000/api/ingredients/v1/ingred");
-            const fillIngredsJson = fillIngreds.json();
-            return fillIngredsJson.result;
-        } catch(error){
-            console.log(error);
-            return null;
-        }
+export const useProxy = (props) => {
+  //bringing list of ingredients from database
+  async function bringIngredPr() {
+    try {
+      const fillIngreds = await fetch(
+        "http://localhost:3000/api/ingredients/v1/ingredlist"
+      );
+      const fillIngredsJson = await fillIngreds.json();
+      return fillIngredsJson.result;
+    } catch (error) {
+      console.log(error);
+      return null;
     }
+  }
 
-    return{
-        bringIngredPr: bringIngredPr
-    }
-}
+  return {
+    bringIngredPr: bringIngredPr,
+  };
+};
